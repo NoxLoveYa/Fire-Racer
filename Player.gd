ef3c2f3a403e27c8e8873e77056340b73ce6b802
NoxLoveYa <ewan.massonet@epitech.eu>
2024-05-18 14:51:11 +0200

    [ADDED] CAR steering (tokyo drift mode)

--- Player.gd
+++ Player.gd
@@ -1,16 +1,17 @@
 extends CharacterBody3D
 
 @export var gravity = -20.0
-@export var engine_power = 6.0
-@export var braking = -9.0
+@export var engine_power = 10.0
+@export var braking = -1.5
 @export var friction = -10.0
 @export var drag = -20.0
-@export var steering_limit = 10.0  # front wheel max turning angle (deg)
-@export var max_speed_reverse = 3.0
+@export var steering_limit = 2.0
+@export var max_speed_reverse = 7.0
 
 # Car state properties
 var acceleration = Vector3.ZERO  # current acceleration
 var steer_angle = 0.0  # current wheel angle
+var steer_axis = Vector3(0, 1, 0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -30,15 +31,18 @@ func get_input():
 	steer_angle = turn * deg_to_rad(steering_limit)
 	acceleration = Vector3.ZERO
 	if Input.is_action_pressed("Accelerate"):
-		acceleration = transform.basis.z * engine_power
+		acceleration += transform.basis.z * engine_power
 	if Input.is_action_pressed("Brake"):
-		acceleration = transform.basis.z * braking
+		acceleration += transform.basis.z * braking
+
+func steer_car():
+	transform.basis = transform.basis.rotated(steer_axis, steer_angle)
 
 func _physics_process(delta):
 	if is_on_floor():
 		get_input()
+		steer_car()
 		apply_friction(delta)
 	velocity.y += gravity * delta
 	velocity += acceleration * delta
 	move_and_slide()
-
